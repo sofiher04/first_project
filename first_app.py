a20061ec3f02ef2fcafbaf827c40b8b767f2f529
sofiher04 <chofifotos@gmail.com>
2024-08-15 10:10:34 -0500

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from PIL import Image
+
 st.title("Regaste tu planta ☘️")
+
 st.header('Si fueras una planta...')
 st.write('¿Qué planta serías?')
+iamge = Image.open('plants.jpg')
+st.image(image, camption = '¡Yo soy todas las plantas!')
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+st.subheader("Ahora usemos 2 columna")
