295c265dda832e6d0ca1030577e60f3e67a63e9e
sofiher04 <chofifotos@gmail.com>
2024-08-15 10:15:13 -0500

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -6,5 +6,7 @@ st.title("Regaste tu planta ☘️")
 st.header('Si fueras una planta...')
 st.write('¿Qué planta serías?')
 image = Image.open('plants.jpg')
-st.image(image, camption = '¡Yo soy todas las plantas!')
+
+st.image(image, caption = '¡Yo soy todas las plantas!')
+
 texto = st.text_input('Escribe algo', 'Este es mi texto')
